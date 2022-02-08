d01bc787bdb79660ddb0c44aaa6c5886c6d83cba
Phillip Bennett <ws21745@bristol.ac.uk>
2022-02-08 14:29:13 +0000

    ow sus ooh um yoontitoonie pin

--- guyD.py
+++ guyD.py
@@ -3,3 +3,9 @@ Set = [1, 2, 3, 4]
 
 for s in Set:
 	print(X**s)
+print("lick my wall you scousie scrummyboy")
+scandytanna = input()
+if scandytanna = "jesus" :
+	print("god")
+else:
+	print("cranbewwydickinson")
